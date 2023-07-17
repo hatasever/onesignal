fb486bd34c1879d9a9fa97d1c454cc78e6143a4f
Hakan ATASEVER <78932248+atasever42@users.noreply.github.com>
2023-07-17 17:49:21 +0300

    onesignal api

---
+++ src/OneSignalServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Atasvr\OneSignal;
+
+use Illuminate\Support\ServiceProvider;
+
+class OneSignalServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/onesignal.php';
+
+        $this->publishes([$configPath => config_path('onesignal.php')], 'config');
+        $this->mergeConfigFrom($configPath, 'onesignal');
+
+        if ($this->app instanceof Laravel\Lumen\Application) {
+            $this->app->configure('onesignal');
+        }
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+
+        $this->app->singleton('onesignal', function ($app) {
+
+
+            $config = isset($app['config']['onesignal']) ? $app['config']['onesignal'] : null;
+            if (is_null($config)) {
+                $config = $app['config']['onesignal'] ?: $app['config']['onesignal::config'];
+            }
+
+            return new OneSignalClient($config['app_id'], $config['rest_api_key'], $config['user_auth_key'] , $config['guzzle_client_timeout']);
+        });
+
+        $this->app->alias('onesignal', 'Atasvr\OneSignal\OneSignalClient');
+    }
+
+    public function provides() {
+        return ['onesignal'];
+    }
+}
